a4322a230e0663af9659aba786046d67c29b102f
SumiAkter2 <akteralisha36@gmail.com>
2023-07-23 16:36:10 +0600

    added mongodb

---
+++ index.js
@@ -0,0 +1,58 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8rhsfz4.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // await client.connect();
+    const productCollection = client.db("moon-tech").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const product = await cursor.toArray();
+
+      res.send({ status: true, data: product });
+    });
+
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+
+      const result = await productCollection.insertOne(product);
+
+      res.send(result);
+    });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await productCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Moon Tech!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
